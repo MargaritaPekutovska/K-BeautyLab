107a357a1fdaa1ebffe4ce883c7ad33136c641f6
MargaritaPekutovska <bondarchuk.margarita@gmail.com>
2024-12-23 16:54:03 +0100

    First commit

---
+++ app/src/main/java/com/margarita_pekutovskaya/k_beautylab/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.margarita_pekutovskaya.k_beautylab
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.margarita_pekutovskaya.k_beautylab.ui.theme.KBeautyLabTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            KBeautyLabTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    KBeautyLabTheme {
+        Greeting("Android")
+    }
+}
