5378203e2c9d50390cb57bf794c81374d7558942
MargaritaPekutovska <bondarchuk.margarita@gmail.com>
2024-12-24 17:22:17 +0100

    Wrapped items into remember block

--- app/src/main/java/com/margarita_pekutovskaya/k_beautylab/compose/CosmeticCatalogueScreen.kt
+++ app/src/main/java/com/margarita_pekutovskaya/k_beautylab/compose/CosmeticCatalogueScreen.kt
@@ -1,13 +1,14 @@
 package com.margarita_pekutovskaya.k_beautylab.compose
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import com.margarita_pekutovskaya.k_beautylab.viewModels.CosmeticCatalogueViewModel
 
 @Composable
 fun CosmeticCatalogueScreen(
-    modifier: Modifier= Modifier,
+    modifier: Modifier = Modifier,
     viewModel: CosmeticCatalogueViewModel
-){
-    val cosmeticItems=viewModel.getCosmeticItems()
+) {
+    val cosmeticItems = remember { viewModel.getCosmeticItems() }
 }
