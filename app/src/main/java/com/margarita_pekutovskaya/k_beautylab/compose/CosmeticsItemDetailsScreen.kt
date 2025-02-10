b7e247534592f6a4d9b550055a40c681474e1ed3
MargaritaPekutovska <bondarchuk.margarita@gmail.com>
2025-02-10 15:14:47 +0100

    changed color app

--- app/src/main/java/com/margarita_pekutovskaya/k_beautylab/compose/CosmeticsItemDetailsScreen.kt
+++ app/src/main/java/com/margarita_pekutovskaya/k_beautylab/compose/CosmeticsItemDetailsScreen.kt
@@ -12,7 +12,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.Button
@@ -31,10 +30,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -58,7 +60,10 @@ fun CosmeticsItemDetailsScreen(
                 TopAppBar(
                     modifier = Modifier.shadow(elevation = 5.dp),
                     title = {
-                        Text(text = stringResource(id = R.string.top_app_bar_title))
+                        Text(
+                            text = stringResource(id = R.string.top_app_bar_title),
+                            fontFamily = FontFamily(Font(R.font.cabin_variable_font_wght))
+                        )
                     },
                     navigationIcon = {
                         IconButton(onClick = onNavigateBack) {
@@ -74,7 +79,18 @@ fun CosmeticsItemDetailsScreen(
                 Column(
                     modifier = Modifier
                         .fillMaxSize()
-                        .background(Color(0xFFFCE4EC))
+                        .fillMaxWidth()
+                        .height(59.dp)
+                        .background(
+                            brush = Brush.verticalGradient(
+                                colors = listOf(
+                                    colorResource(id = R.color.dark_pink),
+                                    colorResource(id = R.color.light_pink),
+                                    colorResource(id = R.color.white),
+                                    colorResource(id = R.color.light_green1)
+                                )
+                            )
+                        )
                         .padding(padding)
                         .padding(16.dp)
                 ) {
@@ -82,16 +98,16 @@ fun CosmeticsItemDetailsScreen(
                         painter = rememberAsyncImagePainter(model = cosmeticItem.imageLink),
                         contentDescription = cosmeticItem.name,
                         modifier = Modifier
-                            .shadow(elevation = 10.dp, shape = RoundedCornerShape(10.dp))
+                            .shadow(elevation = 10.dp)
                             .fillMaxWidth()
                             .height(250.dp)
-                            .clip(RoundedCornerShape(16.dp))
                             .background(Color.White)
                     )
                     Spacer(modifier = Modifier.height(16.dp))
 
                     Text(
                         text = cosmeticItem.name,
+                        fontFamily = FontFamily(Font(R.font.cabin_variable_font_wght)),
                         fontSize = 28.sp,
                         style = MaterialTheme.typography.headlineMedium,
                         fontWeight = FontWeight.Bold
@@ -115,7 +131,7 @@ fun CosmeticsItemDetailsScreen(
                             Text(
                                 text = if (isExpanded) stringResource(id = R.string.top_bar_content_text1)
                                 else stringResource(id = R.string.top_bar_content_text2),
-                                color = Color.Blue,
+                                color = colorResource(id = R.color.dark_pink),
                                 style = MaterialTheme.typography.bodySmall
                             )
                         }
@@ -127,11 +143,11 @@ fun CosmeticsItemDetailsScreen(
                         modifier = Modifier
                             .padding(16.dp)
                             .fillMaxWidth()
-                            .height(46.dp)
+                            .height(40.dp)
                             .align(Alignment.CenterHorizontally),
                         colors = ButtonDefaults.buttonColors(
-                            containerColor = Color.DarkGray,
-                            contentColor = Color.White
+                            containerColor = colorResource(id = R.color.button_color),
+                            contentColor = colorResource(id = R.color.white)
                         )
                     ) {
                         Text(text = stringResource(id = R.string.button_details_text))
